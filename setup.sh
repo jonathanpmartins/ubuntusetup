8eea132223dbcd1873cd8170d25a8d4b21e4439b
Jonathan Martins <web@jonathanmartins.com.br>
2015-02-07 16:19:18 -0200

    Add Sublime Text 3 + Comments

--- setup.sh
+++ setup.sh
@@ -1,6 +1,24 @@
 
+#
+# REPOSITORIES
+#
+
+# sublime text 3 
+sudo add-apt-repository ppa:webupd8team/sublime-text-3
+
+#
+# UPDATE / UPGRADE
+#
 
 sudo apt-get update;
 sudo apt-get upgrade;
 
+#
+#  INSTALL
+#
+
+#git
 sudo apt-get install git;
+
+# sublime text 3 
+sudo apt-get install sublime-text-installer
