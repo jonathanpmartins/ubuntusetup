f93dd8710cc2b8a521fc3d17dcef6c5123f790a1
Jonathan Martins <web@jonathanmartins.com.br>
2015-02-07 16:44:38 -0200

    -y

--- setup.sh
+++ setup.sh
@@ -20,19 +20,18 @@ sudo add-apt-repository ppa:webupd8team/brackets;
 #
 
 sudo apt-get update;
-sudo apt-get upgrade;
+sudo apt-get -y upgrade;
 
 #
 #  INSTALL
 #
 
 #git
-sudo apt-get install git;
-
+sudo apt-get -y install git;
 # sublime text 3
-sudo apt-get install sublime-text-installer;
+sudo apt-get -y install sublime-text-installer;
 # brackets
-sudo apt-get install brackets;
+sudo apt-get -y install brackets;
 
 #
 # SHOW SSH PUBLIC KEY
