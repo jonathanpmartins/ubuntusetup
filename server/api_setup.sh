6eaf2168204fe88f09b7e648ade59bac2f45c59f
Jonathan Martins <web@jonathanmartins.com.br>
2015-05-22 13:00:03 -0300

    Beanstalk

--- server/api_setup.sh
+++ server/api_setup.sh
@@ -14,7 +14,7 @@ sudo apt-get -y upgrade;
 #
 echo "--------------- Installing Tools";
 
-apt_get_packages=( "git" "curl" "nodejs-legacy" "npm" "php5-mysql" "php5-fpm" "php5-cli" "php5-mcrypt" "mcrypt" "php5-curl" "php5-json" "nginx" );
+apt_get_packages=( "git" "curl" "nodejs-legacy" "npm" "php5-mysql" "php5-fpm" "php5-cli" "php5-mcrypt" "mcrypt" "php5-curl" "php5-json" "python-pip" "nginx" );
 
 for i in "${!apt_get_packages[@]}"; do
 	if [ $(dpkg-query -W -f='${Status}' "${apt_get_packages[$i]}" 2>/dev/null | grep -c "ok installed") -eq 0 ];
---
+++ server/beanstalkd_setup.php
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#
+# UPDATE / UPGRADE
+#
+
+echo "--------------- Update + Upgrade";
+
+sudo apt-get update;
+sudo apt-get -y upgrade;
+sudo apt-get -y dist-upgrade;
+
+#
+#  INSTALL
+#
+echo "--------------- Installing Tools";
+
+apt_get_packages=( "curl" "php5-cli" );
+
+for i in "${!apt_get_packages[@]}"; do
+	if [ $(dpkg-query -W -f='${Status}' "${apt_get_packages[$i]}" 2>/dev/null | grep -c "ok installed") -eq 0 ];
+	then
+		echo "--------------- Installing ${apt_get_packages[$i]}";
+		sudo apt-get install -y ${apt_get_packages[$i]};
+	else
+		echo "--------------- '${apt_get_packages[$i]}' already installed";
+	fi
+done
+
+# composer
+if [ ! -f /usr/local/bin/composer ]; then
+	echo "--------------- Installing Composer";
+	curl -sS https://getcomposer.org/installer | php;
+	sudo mv composer.phar /usr/local/bin/composer;
+else
+	echo "--------------- Updating Composer";
+	sudo composer self-update;
+fi
