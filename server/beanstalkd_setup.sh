117fa62874ac6ec999f18200a9a0394a59d94dfc
Jonathan Martins <web@jonathanmartins.com.br>
2015-05-22 13:23:09 -0300

    Beanstalk

--- server/beanstalkd_setup.sh
+++ server/beanstalkd_setup.sh
@@ -15,7 +15,7 @@ sudo apt-get -y dist-upgrade;
 #
 echo "--------------- Installing Tools";
 
-apt_get_packages=( "beanstalkd" "supervisor" "curl" "php5-fpm" "php5-cli" "nginx" );
+apt_get_packages=( "git" "beanstalkd" "supervisor" "curl" "php5-fpm" "php5-cli" "nginx" );
 
 for i in "${!apt_get_packages[@]}"; do
 	if [ $(dpkg-query -W -f='${Status}' "${apt_get_packages[$i]}" 2>/dev/null | grep -c "ok installed") -eq 0 ];
