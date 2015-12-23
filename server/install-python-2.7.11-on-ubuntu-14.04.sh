c67f3b638ba7c3588dffb2177cd8b2bba271a12f
Jonathan Martins <web@jonathanmartins.com.br>
2015-12-23 18:47:51 -0200

    Complete installation

--- server/install-python-2.7.11-on-ubuntu-14.04.sh
+++ server/install-python-2.7.11-on-ubuntu-14.04.sh
@@ -3,18 +3,26 @@
 sudo apt-get update;
 sudo apt-get upgrade;
 
-sudo apt-get install autoconf automake gcc make;
+sudo apt-get install python-pip;
+
+sudo apt-get install autoconf automake gcc make git;
 sudo apt-get install libffi-dev libncurses5-dev openssl patch python-dev python-virtualenv libreadline6-dev libsqlite3-dev libbz2-dev;
 sudo apt-get install libsqlite3-dev libbz2-dev libdb5.3-dev tk8.6-dev libncurses5-dev libssl-dev libgdbm-dev;
 
 wget https://launchpad.net/ubuntu/+archive/primary/+files/python2.7_2.7.11.orig.tar.gz;
-
 tar xf python2.7_2.7.11.orig.tar.gz;
 cd python2.7-2.7.11;
 
-./configure --prefix=/usr/local/lib/python2.7.11 --enable-unicode=ucs4 --enable-ipv6;
+sudo rm -rf /usr/lib/python2.*;
+sudo rm /usr/lib/python2.*;
+sudo rm -rf /usr/local/lib/python2.*;
+sudo rm /usr/local/lib/python2.*;
+
+./configure --prefix=/usr --enable-unicode=ucs4 --enable-ipv6;
 make;
 sudo make install;
 
-sudo rm /usr/bin/python;
-sudo ln -s /usr/local/lib/python2.7.11/bin/python python;
+wget https://bootstrap.pypa.io/get-pip.py;
+sudo python get-pip.py;
+
+sudo pip install virtualenv;
