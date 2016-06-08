675e245ae3664204dc11487210107df4b8290101
Jonathan Martins <web@jonathanmartins.com.br>
2016-06-07 22:20:34 -0300

    8GB swap file

--- server/create-8GB-swap.sh
+++ server/create-8GB-swap.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 df -h;
 
-sudo /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=2048;
+sudo swapoff -a;
+
+sudo /bin/dd if=/dev/zero of=/var/swap.1 bs=512 count=16777216;
 sudo chmod 600 /var/swap.1;
 sudo /sbin/mkswap /var/swap.1;
 sudo /sbin/swapon /var/swap.1;
